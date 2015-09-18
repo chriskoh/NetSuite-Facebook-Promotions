48ab66a09454bd79b08cf8e6b7cdeec2d34d9061
Chris Koh <chris.koh90@gmail.com>
2015-09-17 17:17:14 -0700

    added html formatting

--- fb promotions.js
+++ fb promotions.js
@@ -208,20 +208,20 @@ function function2(){
 	
 	html  = '<html>';
 	html += '<head>';
-	//html += '<script src="https://system.netsuite.com/core/media/media.nl?id=359359&c=811217&h=65afe36a877be122622c&_xt=.js"></script>';  // table sort
-	//html += '<link rel="stylesheet" type="text/css" href="https://system.netsuite.com/core/media/media.nl?id=400790&c=811217&h=c3d4ce9af7d71b7160e3&_xt=.css">'; 
+	html += '<script src="https://system.netsuite.com/core/media/media.nl?id=359359&c=811217&h=65afe36a877be122622c&_xt=.js"></script>';  // table sort
+	html += '<link rel="stylesheet" type="text/css" href="https://system.netsuite.com/core/media/media.nl?id=400790&c=811217&h=c3d4ce9af7d71b7160e3&_xt=.css">'; 
 	//html += '<script src="http://code.jquery.com/jquery-1.5.1.min.js" type="text/javascript"></script>';
 	//html += '<script src="https://system.netsuite.com/core/media/media.nl?id=420986&c=811217&h=4117b836519d6a473b55&_xt=.js" type="text/javascript"></script>';
 	html += '</head>';
 	html += '<body>';
 	
-	html += '<table>';
-	html += '<tr>' +
-				'<td>Customer</td>' +
-				'<td>First Time Customer (Y/N)</td>' +
-				'<td>SO Date</td>' +
-				'<td>Customer Creation Date</td>' +
-				'<td>Promotion</td>';
+	html += '<table id = "myTable">';
+	html += '<tr id="myTRBlue">' +
+				'<td id="myTDBlueData">Customer</td>' +
+				'<td id="myTDBlueData">First Time Customer (Y/N)</td>' +
+				'<td id="myTDBlueData">SO Date</td>' +
+				'<td id="myTDBlueData">Customer Creation Date</td>' +
+				'<td id="myTDBlueData">Promotion</td>';
 	/*
 	// variable length additional promotions based on list of promotions
 	for(var x = 1; x < longest; x++){
@@ -229,27 +229,36 @@ function function2(){
 		html += '<td>Additional Promotion #' + x + '</td>';
 	}
 	*/
-	html +=		'<td>FB Sales Amount</td>' +
-				'<td>Non-FB Sales Amount</td>' +
-				'<td>No Code Sales Amount</td>' +
+	html +=		'<td id="myTDBlueData">FB Sales Amount</td>' +
+				'<td id="myTDBlueData">Non-FB Sales Amount</td>' +
+				'<td id="myTDBlueData">No Code Sales Amount</td>' +
 			'</tr>';
 	
 	var namesposted = new Array();
 		
+	var colorcounter = 0;
+	
 	for(var x = 0; x < allResults.length; x++){
-				
+		
+
+		
 		result = allResults[x];
 
 		if(namesposted.indexOf(result.getText('entity')) > -1){
 			
 			// in array
+
 		}else{
+			
+
 			
 			// not in array
 			
 			var code = result.getText('promocode');
 			
 			if(code.indexOf('fb') != -1 || code.indexOf('FB') != -1){
+				
+
 				
 				if(code != '20offbible'){
 					
@@ -258,12 +267,23 @@ function function2(){
 				    // Initial date check to see if customer is new
 					if( d1.format("m/d/yy") == d2.format("m/d/yy") ){
 						
+						if(colorcounter%2 == 0){
+							color = 'myTDWhiteData';
+							tabbed = 'tabbedodd';
+							color2 = 'odd';
+						}
+						else{
+							color = 'myTDBlueData';
+							color2 = 'even';
+							tabbed = 'tabbed';
+						}
+						
 						html += '<tr>' +
-									'<td>' + result.getText('entity') + '</td>';
+									'<td id="' + color + '">' + result.getText('entity') + '</td>';
 						
 						if(Number(w["firsttime" + result.getValue('entity')]) == Number(1)){
 							
-							html += '<td> Y </td>';
+							html += '<td id="' + color + '"> Y </td>';
 						}else{
 							
 							// New check to see if there were sales made after the initial coupon
@@ -275,9 +295,9 @@ function function2(){
 								html += '<td> N </td>';
 							}
 						}
-							html +=	'<td>' + result.getValue('trandate') + '</td>' +
-									'<td>' + new Date(result.getValue('datecreated', 'customer')).format('mm/dd/yyyy') + '</td>' +
-									'<td>' + result.getText('promocode') + '</td>';
+							html +=	'<td id="' + color + '">' + result.getValue('trandate') + '</td>' +
+									'<td id="' + color + '">' + new Date(result.getValue('datecreated', 'customer')).format('mm/dd/yyyy') + '</td>' +
+									'<td id="' + color + '">' + result.getText('promocode') + '</td>';
 						
 						var currentCode = result.getText('promocode');
 						/*
@@ -297,34 +317,47 @@ function function2(){
 							printed++;
 						}
 						*/
-						html +=		'<td>' + result.getValue('amount') + '</td>';		
+						html +=		'<td id="' + color + '">' + result.getValue('amount') + '</td>';		
 						var tempNonSales = Number(w["nonFBSales" + result.getValue('entity')]) - Number(result.getValue('amount'));
-						html +=		'<td>' + tempNonSales.toFixed(2) + '</td>';	
-						html +=		'<td>' + w["noCodeSales" + result.getValue('entity')].toFixed(2) + '</td>';	
+						html +=		'<td id="' + color + '">' + tempNonSales.toFixed(2) + '</td>';	
+						html +=		'<td id="' + color + '">' + w["noCodeSales" + result.getValue('entity')].toFixed(2) + '</td>';	
 						html +=		'</tr>';
 						
+						colorcounter++;
+
 					}else{
 						
+						if(colorcounter%2 == 0){
+							color = 'myTDWhiteData';
+							tabbed = 'tabbedodd';
+							color2 = 'odd';
+						}
+						else{
+							color = 'myTDBlueData';
+							color2 = 'even';
+							tabbed = 'tabbed';
+						}
+						
 						html += '<tr>' +
-									'<td>' + result.getText('entity') + '</td>';
+									'<td id="' + color + '">' + result.getText('entity') + '</td>';
 						
 						if(Number(w["firsttime" + result.getValue('entity')]) == Number(1)){
 							
-							html += '<td> Y </td>';
+							html += '<td id="' + color + '"> Y </td>';
 						}else{
 							
 							if(new Date(result.getValue('trandate')).format('mm/dd/yyyy') == new Date(result.getValue('datecreated', 'customer')).format('mm/dd/yyyy')){
 								
-								html += '<td> Y </td>';
+								html += '<td id="' + color + '"> Y </td>';
 							}else{
 								
-								html += '<td> N </td>';
+								html += '<td id="' + color + '"> N </td>';
 							}
 						}				
 						
-						html += 	'<td>' + result.getValue('trandate') + '</td>' +
-									'<td>' + new Date(result.getValue('datecreated', 'customer')).format('mm/dd/yyyy') + '</td>' +
-									'<td>' + result.getText('promocode') + '</td>';
+						html += 	'<td id="' + color + '">' + result.getValue('trandate') + '</td>' +
+									'<td id="' + color + '">' + new Date(result.getValue('datecreated', 'customer')).format('mm/dd/yyyy') + '</td>' +
+									'<td id="' + color + '">' + result.getText('promocode') + '</td>';
 						
 						var currentCode = result.getText('promocode');
 						
@@ -345,14 +378,22 @@ function function2(){
 							printed++;
 						}
 						*/
-						html +=		'<td>' + result.getValue('amount') + '</td>';	
+						html +=		'<td id="' + color + '">' + result.getValue('amount') + '</td>';	
 						var tempNonSales = Number(w["nonFBSales" + result.getValue('entity')]) - Number(result.getValue('amount'));
-						html +=		'<td>' + tempNonSales.toFixed(2) + '</td>';	
-						html +=		'<td>' + w["noCodeSales" + result.getValue('entity')].toFixed(2) + '</td>';
+						html +=		'<td id="' + color + '">' + tempNonSales.toFixed(2) + '</td>';	
+						html +=		'<td id="' + color + '">' + w["noCodeSales" + result.getValue('entity')].toFixed(2) + '</td>';
 						html +=		'</tr>';
+						colorcounter++;
+
 					}
 				}
+
+			}else{
+				
+
 			}
+			
+
 		}
 		namesposted = namesposted.concat(result.getText('entity'));
 	}
